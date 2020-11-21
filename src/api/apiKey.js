73449f8aa7662a36f57806c7af18f40553c0f0b9
Jack Zheng <jackzheng784@gmail.com>
2020-11-20 21:40:50 -0500

    Delete apiKey.js

--- src/api/apiKey.js
+++
@@ -1,5 +0,0 @@
-export const DOG_API_KEY = "858deb24-59cd-489f-ac29-8fed4edaa598";
-
-export const PETFINDER_API_KEY =
-  "hPHmptqpW9eQeNhnYdZBBDR4jPUL4Ixw4plpyyJTaUUpIrBGc4";
-export const PETFINDER_SECRET = "e7gQo4rYCp82wR2vqqNxV7w1KTlAlKtdC2PDky2X";
